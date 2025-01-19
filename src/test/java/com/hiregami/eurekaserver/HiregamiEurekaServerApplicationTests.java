f2bed4fb14f150393fb530b234c3f7963dd20865
d.fedorenko <@d.fedorenko>
2025-01-19 13:38:20 +0200

    Initial commit for eureka-server

---
+++ src/test/java/com/hiregami/eurekaserver/HiregamiEurekaServerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.hiregami.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiregamiEurekaServerApplicationTests {
+
+}
